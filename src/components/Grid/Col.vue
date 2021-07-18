66d2c0df738cfec6654c012d6f9e823d8f694a82
sulhwa won <seven1004z@gmail.com>
2021-07-18 18:22:35 +0900

    Grid row, col 컴포넌트 작업

---
+++ src/components/Grid/Col.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="col" :class="cols && `col-${cols}`">
+    <slot></slot>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+
+@Component
+export default class Col extends Vue {
+  @Prop({ type: Number }) cols: number;
+}
+</script>
+
+<style scoped lang="scss">
+$total: 10;
+.col {
+  flex: 0 0 auto;
+  position: relative;
+  width: 100%;
+}
+@for $i from 1 to $total {
+  .col-#{$i} {
+    flex: 0 0 percentage($i / $total);
+    max-width: percentage($i / $total);
+  }
+}
+</style>
---
+++ src/components/Grid/Row.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="row">
+    <slot></slot>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component
+export default class Row extends Vue {}
+</script>
+
+<style scoped lang="scss">
+.row {
+  display: flex;
+  flex-wrap: wrap;
+  margin: -10px;
+  > .col {
+    padding: 10px;
+  }
+}
+</style>
