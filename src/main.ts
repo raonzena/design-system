a0b5f4fc7a3537ebe0f332ea5a49c2bedbb22b95
sulhwa won <seven1004z@gmail.com>
2021-07-18 19:29:48 +0900

    polyfill 추가

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import Vue from 'vue';
 import App from './App.vue';
 import 'normalize.css';
+import '@babel/polyfill';
 
 Vue.config.productionTip = false;
 Vue.filter('numberComma', (value: number) => value.toLocaleString('ko-KR'));
