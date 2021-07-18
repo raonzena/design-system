1c4fea327e886815601b8d0d5dffaab5a11c75b7
sulhwa won <seven1004z@gmail.com>
2021-07-18 18:24:23 +0900

    별점 컴포넌트 작업

---
+++ src/components/StarRate/StarRate.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <template v-for="i in 5">
+      <Star class="star" :isFilled="i <= rate" :key="`rate-${i}`" />
+    </template>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+import Star from '@/components/StarRate/Star.vue';
+
+@Component({
+  components: {
+    Star,
+  },
+})
+export default class StarRate extends Vue {
+  @Prop({ type: Number, default: 0 }) rate: number;
+}
+</script>
+
+<style scoped lang="scss">
+.star {
+  width: 12px;
+  height: 12px;
+}
+</style>
