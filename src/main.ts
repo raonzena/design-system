0b78c571c5e5dad37f3b35a7944c4a62709a1287
sulhwa won <seven1004z@gmail.com>
2021-07-18 18:26:41 +0900

    import normalize.css

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import Vue from "vue";
 import App from "./App.vue";
+import 'normalize.css';
 
 Vue.config.productionTip = false;
 Vue.filter('numberComma', (value: number) => value.toLocaleString('ko-KR'));
