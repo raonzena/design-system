4e3cf4a6aaec9c75b0df79ed6015ec2ba5436891
sulhwa won <seven1004z@gmail.com>
2021-07-18 18:22:06 +0900

    버튼 컴포넌트 작업

---
+++ src/components/Button/Button.vue
@@ -0,0 +1,21 @@
+<template>
+  <button>
+    <slot></slot>
+  </button>
+</template>
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+
+@Component
+export default class Button extends Vue {}
+</script>
+
+<style lang="scss" scoped>
+button {
+  color: #3cbd6e;
+  background-color: white;
+  border: 1px solid #c2c2c2;
+  font-size: 12px;
+  padding: 12px;
+}
+</style>
