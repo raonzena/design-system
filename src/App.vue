0a4c14be7dcf9cc1b4a423ef3c0f9296e3b2aa2f
sulhwa won <seven1004z@gmail.com>
2021-07-18 18:24:01 +0900

    global css 분리

--- src/App.vue
+++ src/App.vue
@@ -32,12 +32,6 @@ export default class App extends Vue {}
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+@import './style/_normalize.scss';
 }
 </style>
